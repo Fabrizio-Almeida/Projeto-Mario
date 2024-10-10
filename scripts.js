66079d665cc693ba9cef7bfd97ebc05a66241e33
Fabrizio-Almeida <fabrizio-almeida@outlook.com>
2024-10-09 21:42:10 -0300

    Ajuste do fale conosco

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 const form = document.querySelector(".Fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
-function cliqueinobotao(){
+function cliqueinobotao() {
     form.style.left = "40%"
     form.style.transform = "translateX(-40)"
     mascara.style.visibility = "visible"
 }
 
-function esconderform(){
+function esconderform() {
     form.style.left = "-300px"
     form.style.transform = "translateX(-0)"
     mascara.style.visibility = "hidden"
 }
+
